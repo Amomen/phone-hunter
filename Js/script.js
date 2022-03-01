f979eff2ecd8916e16b733f02f53d5b2df1e218e
Amomen <amomen424@gmail.com>
2022-03-01 23:28:53 +0600

    almost Done

--- Js/script.js
+++ Js/script.js
@@ -5,7 +5,7 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayPhone(data.data))
+        .then(data => displayPhone(data.data.slice(0, 20)))
 }
 
 const displayPhone = brands => {
@@ -14,26 +14,27 @@ const displayPhone = brands => {
     brands.forEach(brand => {
         const div = document.createElement('div');
         div.classList.add('col');
-        div.innerHTML=` <div  class="card mt-5 ">
+        div.innerHTML = ` <div  class="card mt-5 ">
         <img src="${brand.image}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${brand.phone_name}</h5>
           <p class="card-text">${brand.brand}</p>
-          <button onclick="('${brand.slug}')" class="btn btn-info">Explore</button>
+          <button onclick="loadPhoneDetail('${brand.slug}')" class="btn btn-info">Explore</button>
         </div>
       </div>
     `;
-      result.appendChild(div);
+        result.appendChild(div);
 
-       
+
     });
 }
 
-const loadPhoneDetail=mealId =>{
-    const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+const loadPhoneDetail = brand => {
+    console.log(brand);
+    const url = `https://openapi.programming-hero.com/api/phone/${brand}`
     fetch(url)
-    .then(res => res.json())
-    .then(data =>displayMealDetail(data.meals[0]))
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
 // const displayPhoneDetail = meal => {
 //     console.log(meal)
