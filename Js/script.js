5c2d0af0568a86a8e894a3f90bdcb78a036b33b2
Amomen <amomen424@gmail.com>
2022-03-02 21:57:41 +0600

    almost done

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,4 @@
+//search phone 
 const searchPhone = () => {
     const inputField = document.getElementById('input');
     const searchText = inputField.value;
@@ -7,7 +8,7 @@ const searchPhone = () => {
         .then(res => res.json())
         .then(data => displayPhone(data.data.slice(0, 20)))
 }
-
+/// display phone after search
 const displayPhone = brands => {
     const result = document.getElementById('result');
     // console.log(brands)
@@ -28,14 +29,49 @@ const displayPhone = brands => {
 
     });
 }
-
+//phone Details Api
 const loadPhoneDetail = brand => {
-    console.log(brand);
+    // console.log(brand);
     const url = `https://openapi.programming-hero.com/api/phone/${brand}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayDetails(data.data))
 }
-// const displayPhoneDetail = meal => {
-//     console.log(meal)
-// }
+
+//phone sumery 
+const displayDetails = phones => {
+    // console.log(phones);
+    const showDetails= document.getElementById('showSummery');
+    const div = document.createElement('div');
+        div.classList.add('col-12');
+        div.innerHTML = ` 
+        <div class="row">
+        <div class="col-md-5"><img src="${phones.image}" class="card-img-top" alt="..."></div>
+        <div class="col-md-7">
+        <div  class="card  ">        
+        <div class="card-body">
+        <h5 class="card-title">${phones.mainFeatures.chipSet}</h5>
+          <p class="card-text">${phones.mainFeatures.memory}</p>
+          <p class="card-text">${phones.mainFeatures.storage}</p>
+          <p class="card-text">${phones.mainFeatures.displaySize}</p>
+          <p class="card-text">${phones.mainFeatures.sensors}</p>
+        <h2>Main Features</h2>
+          <h5 class="card-title">${phones.mainFeatures.chipSet}</h5>
+          <p class="card-text">${phones.mainFeatures.memory}</p>
+          <p class="card-text">${phones.mainFeatures.storage}</p>
+          <p class="card-text">${phones.mainFeatures.displaySize}</p>
+          <p class="card-text">${phones.mainFeatures.sensors}</p>
+          
+          
+        </div>
+      </div>
+        </div>
+        </div>
+    `;
+        showSummery.appendChild(div);
+
+    
+}
+
+
+
